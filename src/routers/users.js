d104247f42b59fdaade46efa42ca327ec805b0a0
SeanRichhh <sean-richards@outlook.com>
2022-08-01 16:56:29 +0100

    users completed

---
+++ src/routers/users.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const router = express.Router()
+
+
+const {users} = require("../../data.js")
+
+let id = users.length
+
+router.get("/users", (req,res) => {
+  res.json({users})
+})
+
+router.get("/users/:id", (req,res) => {
+
+  const user = users.find(item => item.id === Number(req.params.id))
+
+  res.json({user})
+})
+
+router.post("/users" , (req,res) => {
+id++
+const user = {...req.body, id : id}
+
+users.push(user)
+
+res.json({user})
+})
+
+router.delete("/:id" , (req, res) => {
+    const user = users.find(item => item.id === Number(req.params.id))
+
+    users.splice(users.indexOf(user), 1)
+    res.json({user})
+})
+
+router.put("/:id", (req,res) => {
+    const user = users.find(item => item.id === Number(req.params.id))
+
+    user.email = req.body.email
+
+    res.json({user})
+})
+
+
+module.exports = router
